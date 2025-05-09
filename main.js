a3f496e4cb68c454e4f2c0355e8cf24d669155e1
LaToya-Renee <latoya.rwill@gmail.com>
2025-05-09 17:24:28 -0400

    Array of sentence structure components filled (15 items each)

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const nouns = [''];
-const verbs = [''];
-const adverbs = [''];
-const adjectives = [''];
+const nouns = ['cat', 'dog', 'monster', 'princess', 'pirate', 'elf', 'reindeer', 'castle', 'zoo', 'box', 'skirt', 'minivan', 'microphone', 'jar of dirt', 'blanket'];
+const verbs = ['jump', 'fly', 'swim', 'play', 'sleep', 'dance', 'sing', 'ride', 'slide', 'climb', 'poke', 'yodel', 'throw', 'cuddle', 'gather'];
+const adverbs = ['quickly', 'joyfully', 'secretly', 'powerfully', 'foolishly', 'innocently', 'kindly', 'devotedly', 'fashionably', 'boastfully', 'hopelessly', 'occasionally', 'gradually', 'hastily', 'audibly'];
+const adjectives = ['happy', 'sad', 'angry', 'crazy', 'afraid', 'excited', 'strong', 'smart', 'pretty', 'fluffy', 'goofy', 'giddy', 'hungry', 'nervous', 'devious'];
 const testString = '';
 const storyOne = '';
